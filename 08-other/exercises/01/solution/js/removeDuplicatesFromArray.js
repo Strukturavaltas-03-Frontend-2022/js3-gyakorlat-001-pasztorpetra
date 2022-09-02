3bb8db4c4442d33e724f9e55674aeb50f07fffcc
Petra <pasztor.petra1998@gmail.com>
2022-09-02 20:02:34 +0200

    1.kész

--- 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
+++ 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
@@ -1,1 +1,6 @@
+// 1. Írj egy függvény kifejezést `removeDuplicatesFromArray` néven, ami a paraméterként megadott tömbből
+// eltávolítja a duplikált elemeket! A tömb csak primítív értékeket tartalmaz!
+// A függvény visszatérési értéke a duplikált elemektől mentes új tömb!
+
+const removeDuplicatesFromArray = (arr) => Array.from(new Set(arr));
 export default removeDuplicatesFromArray;
